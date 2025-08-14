6e8e340216b53c6a28b586699559582aa1a514f6
Nikhil Verma <vermanick75@gmail.com>
2025-08-14 16:49:45 +0530

    corrected config.js and layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes"; 
+import { ThemeProvider } from "next-themes";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,14 +19,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
-          attribute="class"       
-          defaultTheme="system"   
+          attribute="class"
+          defaultTheme="system"
           enableSystem={true}
+          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
