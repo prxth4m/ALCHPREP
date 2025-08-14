1e6566da789c8438367e911bf31e345b997c3898
Nikhil Verma <vermanick75@gmail.com>
2025-08-14 16:57:51 +0530

    corrected  postcss ESM syntax 2.0

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  darkMode: 'class',
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    '@tailwindcss/postcss': {},
   },
-  plugins: [],
 };
 
 export default config;
